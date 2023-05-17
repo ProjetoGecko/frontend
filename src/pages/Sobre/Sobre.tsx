cc2ca508a034128e0b4a159bab0cc2d1b56c78f3
Lais <lais.salesms@gmail.com>
2023-05-17 11:39:21 -0300

    Estrutura da página Sobre

---
+++ src/pages/Sobre/Sobre.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Contato.css'
+import { Grid, Typography } from '@material-ui/core'
+
+function Contato() {
+
+    return (
+        <>
+            <Grid container>
+                <Typography>Sobre</Typography>
+
+                <Grid item container xs={12}>
+                    <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur officiis ullam, ut, architecto aperiam similique unde molestiae rerum voluptatem facilis quaerat fugiat possimus perferendis, praesentium itaque non eligendi vel fuga?</Typography>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Contato
