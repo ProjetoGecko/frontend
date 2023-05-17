85d854afd0ad760a45a1ffe103d04311309ec72e
JULIAVALERIO <juliav286@gmail.com>
2023-05-17 10:31:37 -0300

    Adicionando componentes estaticos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Home from './pages/Home/Home'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1> Frontend </h1>
+      <Home />
     </>
   )
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+import Footer from '../../components/static/Footer/Footer'
+import Navbar from '../../components/static/Navbar/Navbar'
+
+function Home() {
+
+  return (
+    <>
+      <Navbar />
+      <h1> Home </h1>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
