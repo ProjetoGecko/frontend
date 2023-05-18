0bc6b238c987afa2abfa248650cd18d24facef2f
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-05-18 00:14:33 -0300

    Criação do 'Sobre Nós'

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -7,10 +7,10 @@ function Sobre() {
     return (
         <>
             <Grid container>
-                <Typography>Sobre</Typography>
+                <Typography>Sobre nós</Typography>
 
                 <Grid item container xs={12}>
-                    <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur officiis ullam, ut, architecto aperiam similique unde molestiae rerum voluptatem facilis quaerat fugiat possimus perferendis, praesentium itaque non eligendi vel fuga?</Typography>
+                    <Typography>Somos uma e-commerce sustentável na qual usuários podem comprar e vender produtos novos, usados e recicláveis. Este projeto foi idealizado a partir do 12º Objetivo de Desenvolvimento Sustentável da ONU, que aborda o consumo e a produção responsáveis. Escolhemos o Gecko como representante deste tema, devido ao fato do réptil conseguir sobreviver em locais inóspitos, apesar de estar ameaçado de extinção. Com isso, esperamos realizar um trabalho de conscientização e incentivar práticas comerciais que contribuem para um crescimento ecológico, a fim de preservar o nosso Planeta Terra.</Typography>
                 </Grid>
             </Grid>
         </>
