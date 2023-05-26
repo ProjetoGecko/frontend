f4c7db97bd01c2ed219320125ed5144785aca907
Kaique <kaique.ferr@gmail.com>
2023-05-26 10:34:08 -0300

    atualizacao service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -10,6 +10,18 @@ export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
 }
 
 export const login = async(url: any, dados: any, setDado: any) => {
+    
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
+}
+
+export const busca = async(url: any, dados: any, setDado: any) => {
+    
+    const resposta = await api.get(url, dados)
+    setDado(resposta.data)
+}
+
+export const cadastro = async(url: any, dados: any, setDado: any, header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDado(resposta.data)
 }
