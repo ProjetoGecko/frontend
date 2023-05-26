aa9d36d4824eb8b15fe7f75955bdc30ed0d6b492
Lais <lais.salesms@gmail.com>
2023-05-26 14:54:25 -0300

    Cadastrar Categorias funcional

--- src/components/categorias/cadastrar/CadastrarCategoria.tsx
+++ src/components/categorias/cadastrar/CadastrarCategoria.tsx
@@ -1,8 +1,98 @@
-function CadastrarCategoria(){
-    return(
-        <>
-        </>
+import React, { useState, useEffect, ChangeEvent } from 'react'
+import { Container, Typography, TextField, Button } from "@material-ui/core"
+import { useNavigate, useParams } from 'react-router-dom'
+import Categoria from '../../../models/Categoria'
+import { busca, cadastro} from '../../../services/Service'
+import useLocalStorage from 'react-use-localstorage'
+
+function CadastroCategoria() {
+    let navigate = useNavigate()
+
+    const { id } = useParams<{ id: string }>()
+
+    const [token, setToken] = useLocalStorage('token');
+
+    const [categoria, setCategoria] = useState<Categoria>({
+        id: 0,
+        nome: '',
+        descricao: ''
+    })
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+
+            navigate("/login")
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        busca(`/categorias/${id}`, setCategoria, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedCategoria(e: ChangeEvent<HTMLInputElement>) {
+        setCategoria({
+            ...categoria,
+            [e.target.name]: e.target.value,
+        })
+        console.log(categoria)
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (id !== undefined) {
+            // try {
+            //     await put(`/categorias`, categoria, setCategoria, {
+            //         headers: {
+            //             'Authorization': token
+            //         }
+            //     })
+            //     alert('Categoria atualizado com sucesso')
+            // } catch (e) {
+            //     alert(e)
+            // }
+        } else {
+            try {
+                await cadastro(`/categorias`, categoria, setCategoria, {
+                    headers: {
+                        'Authorization': token
+                    }
+                })
+                alert('Categoria cadastrado com sucesso')
+            } catch (e) {
+                alert(e)
+            }
+        }
+        back()
+    }
+
+    function back() {
+        navigate('/Categorias')
+    }
+
+    return (
+        <Container maxWidth="sm" className="topo">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Cadastrar Categoria</Typography>
+                <TextField value={categoria.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedCategoria(e)} id="nome" label="Nome" variant="outlined" name="nome" margin="normal" fullWidth />
+                <TextField value={categoria.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedCategoria(e)} id="descricao" label="Descrição" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <Button type="submit" variant="contained" color="primary">
+                    Finalizar
+                </Button>
+            </form>
+        </Container>
     )
 }
 
-export default CadastrarCategoria
+export default CadastroCategoria
