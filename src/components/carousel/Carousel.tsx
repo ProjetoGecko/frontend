0d10f964d19935cfd645209c6e10033d6e6a2540
Lais <lais.salesms@gmail.com>
2023-06-02 11:57:41 -0300

    Imagens do carrossel transformadas em Import

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -12,6 +12,11 @@ import "./Carousel.css";
 // Importanto Componentes do Swiper
 import { Autoplay, Pagination, Navigation } from "swiper";
 
+import Imagem1 from '../../images/Capa_para_lanchonete_2.png'
+import Imagem2 from '../../images/feira_5.png'
+import Imagem3 from '../../images/Garaffa_Agua_4.png'
+import Imagem4 from '../../images/skincare.png'
+
 function Carrossel() {
     return (
         <>
@@ -33,20 +38,20 @@ function Carrossel() {
             >
 
                 <SwiperSlide>
-                    <img src="src\images\Capa_para_lanchonete_2.png" />
+                    <img src={Imagem1} />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <img src="src\images\feira_5.png" />
+                    <img src={Imagem2} />
                 </SwiperSlide>
                 
                 <SwiperSlide>
-                    <img  src="src\images\Garaffa_Agua_4.png" alt="Imagem" />
+                    <img  src={Imagem3} />
                 </SwiperSlide>
 
                
                 <SwiperSlide>
-                    <img src="src\images\skincare.png" />
+                    <img src={Imagem4} />
                 </SwiperSlide>
 
             </Swiper>
