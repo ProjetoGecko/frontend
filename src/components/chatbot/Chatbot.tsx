12a0bf5b1b5e5db47899468654b4ef3198fefd81
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-06-13 23:18:05 -0300

    Arrumando A ortografia e  centralizando o chatbot ao centro da tela

--- src/components/chatbot/Chatbot.tsx
+++ src/components/chatbot/Chatbot.tsx
@@ -10,7 +10,7 @@ import './Chatbot.css'
 function MycChatbot() {
 
     const theme = {
-
+        
         background: '#F6F4EB',
         fontFamily: 'Poppins',
         headerBgColor: '#55A630',
@@ -21,20 +21,17 @@ function MycChatbot() {
         userBubbleColor: '#55A630',
         userFontColor: 'white',
         alignItems: "center",
-        botAvatar: botImage
+        botAvatar: botImage,
+    
 
     };
-
-
-
-
 
 
     const steps = [
 
         {
             id: "Greet",
-            message: "Olá, sou Gecko assistente do Gecko! Bem vindo!",
+            message: "Olá, sou o Gecko, seu assistente! Bem-vindo!",
             trigger: "Done",
         },
 
@@ -58,20 +55,20 @@ function MycChatbot() {
             id: "issues",
             options: [
                 { value: "oqGecko", label: "O que é o Gecko?", trigger: "oqGecko" },
-                { value: "criadores", label: "Quem são os criadores do Gecko?", trigger: "criadores" },
+                { value: "pqGecko", label: "Por que comprar no Gecko?", trigger: "pqGecko" },
             ]
         },
         {
             id: "oqGecko",
             message:
-                "É uma ecommerce com o objetivo de Ajudar o mundo",
+                "O Gecko é um e-commerce que tem como objetivo ajudar a salvar o mundo por meio de práticas sustentáveis. Ele oferece uma ampla variedade de produtos de diferentes categorias, como moda, eletrônicos, artigos para casa e muito mais. O Gecko se preocupa com o meio ambiente, promovendo a utilização de embalagens eco-friendly, selecionando produtos sustentáveis e buscando parcerias com organizações ambientais. Além disso, o Gecko tem o compromisso de educar e conscientizar seus clientes sobre a importância da sustentabilidade. Comprando no Gecko, você estará contribuindo para um futuro mais sustentável e apoiando um e-commerce ético e responsável.",
             trigger: "newQuestion",
         },
 
         {
-            id: "criadores",
+            id: "pqGecko",
             message:
-                "O Gecko foi criado por: Bruno Kauã, Keteleyn Medina, Kaique Ferreira, Lais Sales, Leticia Oliveira e Julia Valerio",
+                "Ao escolher o Gecko, você apoia um e-commerce sustentável e ético, que se preocupa com o meio ambiente e busca promover práticas responsáveis, como o uso de embalagens eco-friendly, a oferta de produtos sustentáveis, parcerias com organizações ambientais e a promoção da educação e conscientização sobre sustentabilidade",
             trigger: "newQuestion",
         },
         {
@@ -89,7 +86,7 @@ function MycChatbot() {
         },
         {
             id: 'end-message',
-            message: 'Obrigada!Para mais informações entre em contato:email do gecko ❤',
+            message: 'Obrigado! Para mais informações, entre em contato através do email: projetogecko@gmail.com ❤',
             end: true,
         },
 
@@ -97,10 +94,13 @@ function MycChatbot() {
     return (
 
         <ThemeProvider theme={theme}>
+              <div className="chatbot-container">
             <ChatBot
                 steps={steps}
                 botAvatar={theme.botAvatar} />
+                </div>
         </ThemeProvider>
+        
     );
 
 }
